c9d7467bd55e7644b772c24bf799cd6b6211f90a
alnreddy814242 <92281848+alnreddy814242@users.noreply.github.com>
2022-02-11 16:00:00 +0530

    Update docker_login.sh

--- myscripts/docker_login.sh
+++ myscripts/docker_login.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Logging in to Amazon ECR..."
-aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 116117548704.dkr.ecr.us-east-2.amazonaws.com
+aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 436767166115.dkr.ecr.us-east-2.amazonaws.com
